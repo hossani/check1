38a1c4727d13b69814e99bf89331e3006f54a4d7
hossani <hamza.hossani.001@gmail.com>
2023-05-20 14:12:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Form</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">About</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </header>
+
+      <main>
+        <div className="row">
+          <div className="col-md-6">
+            <h1>Welcome to my app</h1>
+            <p>Simple application built with React and Bootstrap.</p>
+          </div>
+          <div className="col-md-6">
+            <form link="">
+              <div className="mb-3">
+                <label htmlFor="firstName" className="form-label">First name</label>
+                <input type="text" className="form-control" id="firstName" />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="lastName" className="form-label">Last name</label>
+                <input type="text" className="form-control" id="lastName" />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="email" />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="message" className="form-label">Message</label>
+                <textarea className="form-control" id="message" rows="3"></textarea>
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+      </main>
+
+      <footer className="mt-5">
+        <p>@ {new Date().getFullYear()} React App. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
